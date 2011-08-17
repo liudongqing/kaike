1588df7f62d4889d17a42825fc46487d179842bb
Liu DongqingLiu DongqingLiu Dongqing <liudongqing@gmail.com>
2011-08-18 00:22:26 +0800

    Finished course and lecture view

--- course/models.py
+++ course/models.py
@@ -11,19 +11,42 @@ class School(models.Model):
     def __unicode__(self):
         return self.name
 
-class Teacher(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField()
+    douban_id = models.CharField(max_length=40)
 
     def __unicode__(self):
         return self.name
 
 class Course(models.Model):
     title = models.CharField(max_length=100)
-    teachers = models.ManyToManyField(Teacher)
+    introduction = models.TextField()
+    teachers = models.ManyToManyField(User)
+    poster = models.CharField(max_length=100)
     school= models.ForeignKey(School)
     open_date= models.DateField()
     
+    def __unicode__(self):
+        return self.title
+
+class Register(models.Model):
+    course = models.ForeignKey(Course)
+    student = models.ForeignKey(User)
+    reg_type = models.IntegerField()
+
+    def __unicode__(self):
+        return "{} register {}".format(student,course)
+
+
+class Lecture(models.Model):
+    title = models.CharField(max_length=100)
+    poster = models.CharField(max_length=100)
+    teacher = models.ForeignKey(User)
+    introduction = models.CharField(max_length=280,null=True)
+    course = models.ForeignKey(Course)
+    open_time= models.DateTimeField()
+    duration = models.IntegerField()
 
     def __unicode__(self):
         return self.title
@@ -32,6 +55,7 @@ class Reading(models.Model):
     title = models.CharField(max_length=100)
     catalog = models.IntegerField()
     serialno = models.CharField(max_length=20)
+    lecture = models.ForeignKey(Lecture)
 
     def __unicode__(self):
         return self.title
@@ -40,17 +64,8 @@ class Assignment(models.Model):
      title = models.CharField(max_length=140)
      description = models.CharField(max_length=500)
      due_date = models.DateField()
-
+     lecture = models.ForeignKey(Lecture)
+     
      def __unicode__(self):
-         return title
+         return self.title
 
-class Lecture(models.Model):
-    title = models.CharField(max_length=100)
-    teacher = models.ForeignKey(Teacher)
-    open_time= models.DateTimeField()
-    duration = models.IntegerField()
-    assignments = models.ManyToManyField(Assignment)
-    readings =  models.ManyToManyField(Reading)
-
-    def __unicode__(self):
-        return title
--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from kaike.views import Home
 from django.conf import settings
 from django.contrib import admin
 from django.views.generic.list_detail import object_detail
-from kaike.course.models import Course
+from kaike.course.models import *
 
 
 admin.autodiscover()
@@ -16,11 +16,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     ('^$',Home),
 
-    (r'^image/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Users/wuyouz/Projects/kaike/image'}),
+    (r'^image/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Users/wuyouz/GithubRepo/kaike/image'}),
     (r'^admin/', include(admin.site.urls)),
     (r'^admin/course/()', include(admin.site.urls)),
     (r'^course/(?P<object_id>\d+)/$', object_detail, {'template_name': 'course_details.html','queryset': Course.objects.all()}),
+    (r'^lecture/(?P<object_id>\d+)/$', object_detail, {'template_name': 'lecture_details.html','queryset': Lecture.objects.all()}),
+    (r'^assignment/(?P<object_id>\d+)/$', object_detail, {'template_name': 'assignment_details.html','queryset': Assignment.objects.all()}),
                        
+               
                        
     # Example:
     # (r'^kaike/', include('kaike.foo.urls')),
